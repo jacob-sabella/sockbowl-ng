9cfefd264ef4d170db1d2a8b568d9113d3472646
Ethan Miller <soulofaton@gmail.com>
2024-03-21 23:42:21 -0500

    updating port number to match default in sockbowl-game

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/api/v1/session',
-  wsUrl: 'ws://localhost:8080/sockbowl-game'
+  apiUrl: 'http://localhost:7000/api/v1/session',
+  wsUrl: 'ws://localhost:7000/sockbowl-game'
 };
