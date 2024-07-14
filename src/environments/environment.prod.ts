0239dd6c2a5e9c95b00accca25a51a8c9473184c
Jacob Sabella <jacobsabella@outlook.com>
2024-07-14 13:27:51 -0500

    Fizx port in websocket url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   sockbowlGameApiUrl: 'http://alpha.sockbowl.com:7000/api/v1/session',
   sockbowlQuestionsApiUrl: 'http://alpha.sockbowl.com:7001/api/v1/guestion',
-  wsUrl: 'ws://alpha.sockbowl.com/sockbowl-game'
+  wsUrl: 'ws://alpha.sockbowl.com/sockbowl-game:7000'
 };
