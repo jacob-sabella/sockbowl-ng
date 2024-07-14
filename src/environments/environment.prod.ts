e3501a24954b4b852a9a2fa7bf3436099ca61588
Jacob Sabella <jacobsabella@outlook.com>
2024-07-14 14:14:20 -0500

    Ports. How could they work

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   sockbowlGameApiUrl: 'http://alpha.sockbowl.com:7000/api/v1/session',
   sockbowlQuestionsApiUrl: 'http://alpha.sockbowl.com:7001/api/v1/guestion',
-  wsUrl: 'ws://alpha.sockbowl.com/sockbowl-game:7000'
+  wsUrl: 'ws://alpha.sockbowl.com:7000/sockbowl-game'
 };
