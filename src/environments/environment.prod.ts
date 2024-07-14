9d1894665183d59e3d1f0f166537bcc0a95d3b61
Jacob Sabella <jacobsabella@outlook.com>
2024-07-14 14:28:53 -0500

    Guestion?

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   sockbowlGameApiUrl: 'http://alpha.sockbowl.com:7000/api/v1/session',
-  sockbowlQuestionsApiUrl: 'http://alpha.sockbowl.com:7001/api/v1/guestion',
+  sockbowlQuestionsApiUrl: 'http://alpha.sockbowl.com:7001/api/v1/packets',
   wsUrl: 'ws://alpha.sockbowl.com:7000/sockbowl-game'
 };
