0dcaecd66774d2e049eea6ab275248436159924c
Jacob Sabella <jacobsabella@outlook.com>
2024-07-14 13:32:59 -0500

    Add more clarity to commands to run

--- docker-image-push.sh
+++ docker-image-push.sh
@@ -1,3 +1,4 @@
+ng build --configuration production
 docker build -t sockbowl-ng .
 docker tag sockbowl-ng:latest 339712961919.dkr.ecr.us-east-1.amazonaws.com/sockbowl-ng:latest
 docker push 339712961919.dkr.ecr.us-east-1.amazonaws.com/sockbowl-ng:latest
