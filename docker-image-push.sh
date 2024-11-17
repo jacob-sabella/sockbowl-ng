c9aa6cc5304a3f5cd8906853eacb0747454e6ae1
Jacob Sabella <jacobsabella@outlook.com>
2024-11-17 16:22:55 -0600

    Tests

--- docker-image-push.sh
+++
@@ -1,4 +0,0 @@
-npm run buildprod
-docker build -t sockbowl-ng .
-docker tag sockbowl-ng:latest 339712961919.dkr.ecr.us-east-1.amazonaws.com/sockbowl-ng:latest
-docker push 339712961919.dkr.ecr.us-east-1.amazonaws.com/sockbowl-ng:latest
