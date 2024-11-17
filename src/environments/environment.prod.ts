73cc39822d267f0332056c052ec1f270cd97c0fa
Jacob Sabella <jacobsabella@outlook.com>
2024-11-17 16:16:14 -0600

    Change to match kube env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  sockbowlGameApiUrl: 'http://alpha.sockbowl.com:7000/api/v1/session',
-  sockbowlQuestionsApiUrl: 'http://alpha.sockbowl.com:7001/api/v1/packets',
-  wsUrl: 'ws://alpha.sockbowl.com:7000/sockbowl-game'
+  sockbowlGameApiUrl: 'http://game.sockbowl.localhost/api/v1/session',
+  sockbowlQuestionsApiUrl: 'http://questions.sockbowl.localhost/api/v1/packets',
+  wsUrl: 'ws://game.sockbowl.localhost/sockbowl-game'
 };
