243727b1136843585011610ea8767c82c293561b
Jacob Sabella <jacobsabella@outlook.com>
2025-12-24 21:27:30 -0600

    Keep auth on

--- src/assets/config.template.js
+++ src/assets/config.template.js
@@ -1,17 +1,19 @@
 // Runtime configuration template
 // Environment variables will be substituted at container startup
 window.__env = {
-  sockbowlGameApiUrl: '${APP_PROTOCOL}://${APP_HOST}:${SOCKBOWL_GAME_PORT}/api/v1/session',
-  sockbowlQuestionsApiUrl: '${APP_PROTOCOL}://${APP_HOST}:${SOCKBOWL_QUESTIONS_PORT}/',
-  wsUrl: '${WS_PROTOCOL}://${APP_HOST}:${SOCKBOWL_GAME_PORT}/sockbowl-game',
-  authEnabled: ${AUTH_ENABLED},
+  sockbowlGameApiUrl:
+    "${APP_PROTOCOL}://${APP_HOST}:${SOCKBOWL_GAME_PORT}/api/v1/session",
+  sockbowlQuestionsApiUrl:
+    "${APP_PROTOCOL}://${APP_HOST}:${SOCKBOWL_QUESTIONS_PORT}/",
+  wsUrl: "${WS_PROTOCOL}://${APP_HOST}:${SOCKBOWL_GAME_PORT}/sockbowl-game",
+  authEnabled: true,
   keycloak: {
-    issuer: '${APP_PROTOCOL}://${APP_HOST}:${KEYCLOAK_PORT}/realms/sockbowl',
-    clientId: 'sockbowl-game',
-    redirectUri: window.location.origin + '/game-session',
-    scope: 'openid profile email',
-    responseType: 'code',
-    showDebugInformation: ${AUTH_ENABLED} === false,
-    requireHttps: '${APP_PROTOCOL}' === 'https'
-  }
+    issuer: "${APP_PROTOCOL}://${APP_HOST}:${KEYCLOAK_PORT}/realms/sockbowl",
+    clientId: "sockbowl-game",
+    redirectUri: window.location.origin + "/game-session",
+    scope: "openid profile email",
+    responseType: "code",
+    showDebugInformation: false,
+    requireHttps: "${APP_PROTOCOL}" === "https",
+  },
 };
